2e6c2af564ae0fc61d26bddb502a7517cbdd63d8
Max Aubain <max.aubain@gmail.com>
2019-07-06 05:14:49 +0200

    Refactor user can create article feature test and added basic steps

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,9 +1,22 @@
-When("I visit the site") do
-  visit root_path
-end
-
 Given("the following articles exists") do |table|
     table.hashes.each do |article|
       Article.create!(article)
     end
 end
+
+When("I visit the landing page") do
+  visit root_path
+end
+
+When("I click {string}") do |text|
+  click_link_or_button(text)
+end
+
+When("I fill in {string} with {string}") do |field, content|
+  fill_in field, with: content
+end
+
+Then("I should be on Learning Rails 5 page")
+  pending #visit page
+end
+
